eeb16bbacf025de4986b9e8b44c967d0bbf58451
glenrioariesto <glenrioariesto@gmail.com>
2026-08-28 09:12:47 +0700

    feat: add GitHub Actions workflow for deploying static content to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/dasar-dasar-visual/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
